0fa88980b399cddd7ae02824fa7c8bd0e55f547b
Rogério R. Alcântara <rogerio.alcantara@clinicorp.com>
2023-11-18 16:01:47 -0300

    feat(client): Listen to the WebSocket
    And send the client x and y location to the WebSocket.

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,3 +1,25 @@
+import throttle from 'lodash.throttle';
+import { useEffect, useRef } from 'react';
+import useWebSocket from 'react-use-websocket';
+
+const WS_URL = 'ws://127.0.0.1:8000';
+const THROTTLE = 50;
+
 export function Home({ username }) {
+  const { sendJsonMessage } = useWebSocket(WS_URL, {
+    queryParams: { username },
+  });
+
+  const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE));
+
+  useEffect(() => {
+    window.addEventListener('mousemove', (e) => {
+      sendJsonMessageThrottled.current({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    });
+  }, [sendJsonMessageThrottled]);
+
   return <h1>Hello, {username}</h1>;
 }
