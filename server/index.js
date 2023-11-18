3b89aa2e95a87d8c157e4a86545660c31d4a3d64
Rogério R. Alcântara <rogerio.alcantara@clinicorp.com>
2023-11-18 13:40:43 -0300

    feat(ws): Add WebSocketServer connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const http = require('http');
+const url = require('url');
+const { WebSocketServer } = require('ws');
+
 const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
 const PORT = 8000;
+
+// ws://localhost:8000?username=Alex
+wsServer.on('connection', (conn, req) => {
+  const { username } = url.parse(req.url, true).query;
+  console.log(`User ${username} connected`);
+});
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
